6d1930e08aa9d5c22ae15f6aefeafbcca90eb95c
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-08-19 11:46:20 +0530

    Add files via upload

---
+++ 150Q/Greedy/GasStation.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int gas(vector<int>gas,vector<int>cost){
+    int totalGas=0,totalCost=0;
+    int CurrGas=0,start=0;
+    for(int val:gas){
+        totalGas+=val;
+    }
+    for(int val:cost){
+        totalCost+=val;
+
+    }
+    if(totalGas<totalGas) return -1;
+
+    for(int i=0;i<gas.size();i++){
+        CurrGas+=gas[i]-cost[i];
+        if(CurrGas<0)[
+            start=i+1;
+            Currgas=0;
+        ]
+    
+    }
+    return start;
+}
