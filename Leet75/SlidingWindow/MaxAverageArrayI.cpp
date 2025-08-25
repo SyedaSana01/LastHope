2ad4fc7138df268574b63de225b9877c484fe30f
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-08-25 09:43:01 +0530

    Add files via upload

---
+++ Leet75/SlidingWindow/MaxAverageArrayI.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        long long sum=0;
+        int n= nums.size();
+
+        for(int i=0;i<k;i++){
+            sum+=nums[i];
+        }
+
+        long long maxSum=sum;
+
+        for(int i=k;i<n;i++){
+            sum+=nums[i]-nums[i-k];
+            maxSum=max(maxSum,sum);
+        }
+        return (double)maxSum/k;
+        
+    }
+};
