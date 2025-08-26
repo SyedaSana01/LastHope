335ad836c7f617fdaf36f701f097f4e767e68a3d
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-08-26 10:44:26 +0530

    Update IsSubsequence.cpp

--- Leet75/2pointers/IsSubsequence.cpp
+++ Leet75/2pointers/IsSubsequence.cpp
@@ -11,3 +11,32 @@ public:
     }
 
 };
+
+
+Or you can also try 
+
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m=s.length();
+        int n=t.length();
+
+        int i=0,j=0;
+        if(m>n) return false;
+
+        while(i<m && j<n){
+            
+              if(s[i]==t[j]) 
+            {
+                i++;
+                j++;
+            }
+            j++;
+        }
+
+        return i==m;
+              
+        
+    }
+};
