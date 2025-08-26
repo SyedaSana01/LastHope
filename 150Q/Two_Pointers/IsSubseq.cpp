5eec7ed381a86e685dbc38a737a3c5cb640ebcdf
Syeda Suhasana <164773519+SyedaSana01@users.noreply.github.com>
2025-08-26 10:43:23 +0530

    Update IsSubseq.cpp

--- 150Q/Two_Pointers/IsSubseq.cpp
+++ 150Q/Two_Pointers/IsSubseq.cpp
@@ -1,18 +1,24 @@
-#include<bits/stdc++.h>
-using namespace std;
-bool Sub(string s,string t){
-    int m=s.length();
-    int n=t.length();
-    int i=0,j=n;
-    if(m>n) return false;
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m=s.length();
+        int n=t.length();
 
-    while(m<n){
-        
-        if(s[i]==t[j]){
-            i++,j++;
+        int i=0,j=0;
+        if(m>n) return false;
+
+        while(i<m && j<n){
+            
+              if(s[i]==t[j]) 
+            {
+                i++;
+                j++;
+            }
+            j++;
         }
 
+        return i==m;
+              
+        
     }
-    return i==m;
-
-}
+};
